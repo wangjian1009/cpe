48e6a81519951b374914da83df3fdc6d7b1d1b69
Loki <wangjian1009@gmail.com>
2018-05-09 11:47:06 +0800

    update

--- src/utils/ringbuffer.c
+++ src/utils/ringbuffer.c
@@ -219,7 +219,7 @@ ringbuffer_block_data(ringbuffer_t rb, ringbuffer_block_t blk, int skip, void **
 }
 
 int ringbuffer_block_len(ringbuffer_t rb, ringbuffer_block_t blk, int skip) {
-    return blk->length - sizeof(struct ringbuffer_block) - blk->offset;
+    return blk->length - sizeof(struct ringbuffer_block) - blk->offset - skip;
 }
 
 int ringbuffer_block_total_len(ringbuffer_t rb, ringbuffer_block_t blk) {
