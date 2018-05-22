137d77daede4abd4009d2f736388b77f66b2f0e4
Loki <wangjian1009@gmail.com>
2018-05-22 14:57:57 +0800

    update

--- include/cpe/utils/array.h
+++ include/cpe/utils/array.h
@@ -20,7 +20,7 @@ void cpe_raw_array_fini(cpe_raw_array_t array);
 void cpe_raw_array_set_callback_data(cpe_raw_array_t array, void *user_data, void (*free_user_data)(mem_allocrator_t, void *));
 
 void cpe_raw_array_set_init(cpe_raw_array_t array, void (*init)(void *user_data, void *value));
-void cpe_raw_array_set_done(cpe_raw_array_t array, void (*done)(void *user_data, void *value));
+void cpe_raw_array_set_fini(cpe_raw_array_t array, void (*done)(void *user_data, void *value));
 void cpe_raw_array_set_reuse(cpe_raw_array_t array, void (*reuse)(void *user_data, void *value));
 void cpe_raw_array_set_remove(cpe_raw_array_t array, void (*remote)(void *user_data, void *value));
 
@@ -51,17 +51,17 @@ int cpe_raw_array_copy(
 #define cpe_array_is_empty(arr)  ((arr)->size == 0)
 #define cpe_array_to_raw(arr)    ((cpe_raw_array_t ) (void *) (arr))
 
-#define cpe_array_init(arr) \
-    (cpe_raw_array_init(cpe_array_to_raw(arr), cpe_array_element_size(arr)))
-#define cpe_array_done(arr) \
-    (cpe_raw_array_done(cpe_array_to_raw(arr)))
+#define cpe_array_init(arr, alloc)                                            \
+    (cpe_raw_array_init(cpe_array_to_raw(arr), (alloc), cpe_array_element_size(arr)))
+#define cpe_array_fini(arr) \
+    (cpe_raw_array_fini(cpe_array_to_raw(arr)))
 
 #define cpe_array_set_callback_data(arr, ud, fud) \
     (cpe_raw_array_set_callback_data(cpe_array_to_raw(arr), (ud), (fud)))
 #define cpe_array_set_init(arr, i) \
     (cpe_raw_array_set_init(cpe_array_to_raw(arr), (i)))
-#define cpe_array_set_done(arr, d) \
-    (cpe_raw_array_set_done(cpe_array_to_raw(arr), (d)))
+#define cpe_array_set_fini(arr, d) \
+    (cpe_raw_array_set_fini(cpe_array_to_raw(arr), (d)))
 #define cpe_array_set_reuse(arr, r) \
     (cpe_raw_array_set_reuse(cpe_array_to_raw(arr), (r)))
 #define cpe_array_set_remove(arr, r) \
