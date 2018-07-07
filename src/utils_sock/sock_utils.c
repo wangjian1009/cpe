16773fa9555bdcfe4c1a7adb1edcf6d94eceacc9
Lokiwang <wangjian1009@gmail.com>
2018-07-07 12:20:05 +0800

    warning resolve

--- src/utils_sock/sock_utils.c
+++ src/utils_sock/sock_utils.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include "cpe/pal/pal_string.h"
+#include "cpe/pal/pal_stdio.h"
 #include "cpe/pal/pal_strings.h"
 #include "cpe/pal/pal_socket.h"
 #include "cpe/pal/pal_platform.h"
