c7d8f4525237e30ac1779381fd12957c16889662
Lokiwang <wangjian1009@gmail.com>
2018-09-19 16:18:48 +0800

    忽略yajl编译告警

--- buildtools/depends-yajl.mk
+++ buildtools/depends-yajl.mk
@@ -1,7 +1,7 @@
 yajl_base:=$(call my-dir)/../depends/yajl
 yajl_output:=$(OUTPUT_PATH)/lib/libyajl.a
 yajl_cpp_flags:=-I$(yajl_base)/include
-yajl_c_flags:=-Wno-implicit-function-declaration
+yajl_c_flags:=-Wno-implicit-function-declaration -Wno-enum-conversion
 yajl_src:=$(wildcard $(yajl_base)/src/*.c)
 $(eval $(call def_library,yajl))
 
