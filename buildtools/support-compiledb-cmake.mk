aa11fbe4974f70019ad5d7dcf25b217adb36855e
Lokiwang <wangjian1009@gmail.com>
2019-07-03 14:31:18 +0800

    compiledb-cmake支持设置参数

--- buildtools/support-compiledb-cmake.mk
+++ buildtools/support-compiledb-cmake.mk
@@ -3,7 +3,7 @@
 compiledb:
 	mkdir -p $(COMPILEDB_ROOT)/build/cmake \
 	&& cd $(COMPILEDB_ROOT)/build/cmake \
-    && cmake $(COMPILEDB_ROOT) -BDebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
+    && cmake $(COMPILEDB_ROOT) -BDebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES $(COMPILEDB_CMAKE_ARGS)
 	bash -c 'if [ ! -f $(COMPILEDB_ROOT)/compile_commands.json ]; then \
 		ln -s build/cmake/Debug/compile_commands.json $(COMPILEDB_ROOT)/compile_commands.json; \
 	fi'
