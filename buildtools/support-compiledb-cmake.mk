63e38067ea3983b6fac343c3230b9ce67ed16a4b
Lokiwang <wangjian1009@gmail.com>
2019-07-17 21:59:40 +0800

    清理不需要的support-android.cmake

--- buildtools/support-compiledb-cmake.mk
+++ buildtools/support-compiledb-cmake.mk
@@ -1,9 +1,8 @@
 .PHONY: compiledb
 
 compiledb:
-	mkdir -p $(COMPILEDB_ROOT)/build/cmake \
-	&& cd $(COMPILEDB_ROOT)/build/cmake \
-    && cmake $(COMPILEDB_ROOT) -BDebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES $(COMPILEDB_CMAKE_ARGS)
-	bash -c 'if [ ! -f $(COMPILEDB_ROOT)/compile_commands.json ]; then \
-		ln -s build/cmake/Debug/compile_commands.json $(COMPILEDB_ROOT)/compile_commands.json; \
-	fi'
+	mkdir -p $(COMPILEDB_ROOT)/$(if $(COMPILEDB_OUTPUT),$(COMPILEDB_OUTPUT),build/cmake) \
+	&& cd $(COMPILEDB_ROOT)/$(if $(COMPILEDB_OUTPUT),$(COMPILEDB_OUTPUT),build/cmake) \
+    && cmake $(COMPILEDB_ROOT) -BDebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES $(COMPILEDB_CMAKE_ARGS) \
+	&& rm -f $(COMPILEDB_ROOT)/compile_commands.json \
+	&& ln -s $(if $(COMPILEDB_OUTPUT),$(COMPILEDB_OUTPUT),build/cmake)/Debug/compile_commands.json $(COMPILEDB_ROOT)/compile_commands.json
