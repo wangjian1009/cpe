b14ac87873b65455d88b99fcb5ea2b31f975fd5a
Lokiwang <wangjian1009@gmail.com>
2019-09-01 14:32:08 +0800

    增加json转义打印的代码

--- utils_json/include/yajl_utils_types.h
+++ utils_json/include/yajl_utils_types.h
@@ -9,7 +9,10 @@ extern "C" {
 typedef struct cpe_yajl_tree_gen * cpe_yajl_tree_gen_t;
 
 const char * cpe_yajl_tree_type_str(int type);
-    
+
+void cpe_yajl_stream_escape_str_range(write_stream_t ws, const char * msg, const char * msg_end);
+void cpe_yajl_stream_escape_str(write_stream_t ws, const char * msg);
+
 #ifdef __cplusplus
 }
 #endif
---
+++ utils_json/src/yajl_utils_stream.c
@@ -0,0 +1,34 @@
+#include "cpe/pal/pal_string.h"
+#include "cpe/utils/stream.h"
+#include "yajl_utils_types.h"
+
+void cpe_yajl_stream_escape_str_range(write_stream_t ws, const char * msg, const char * msg_end) {
+    const char * block_begin = msg;
+    const char * block_end = msg;
+    
+    while(block_end < msg_end) {
+        switch(*block_end) {
+        case '"':
+            if (block_begin < block_end) {
+                stream_write(ws, block_begin, block_end - block_begin);
+            }
+            stream_putc(ws, '\\');
+            stream_putc(ws, *block_end);
+            block_end++;
+            block_begin = block_end;
+            break;
+        default:
+            block_end++;
+            break;
+        }
+    }
+    
+    if (block_begin < block_end) {
+        stream_write(ws, block_begin, block_end - block_begin);
+    }
+}
+
+void cpe_yajl_stream_escape_str(write_stream_t ws, const char * msg) {
+    cpe_yajl_stream_escape_str_range(ws, msg, msg + strlen(msg));
+}
+
