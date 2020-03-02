54064058d29162f4b1ee17da9335cf54ce4ce6b4
Loki <wangjian1009@gmail.com>
2020-03-02 17:41:27 +0800

    add support for msys

--- buildtools/check-system.mk
+++ buildtools/check-system.mk
@@ -3,8 +3,10 @@ ORIGN_OS_NAME:=$(shell uname -s)
 ifeq ($(ORIGN_OS_NAME),Linux)
 ifeq ($(shell uname -p),x86_64)
 OS_NAME:=linux64
+OS_ARCH:=amd64
 else
 OS_NAME:=linux32
+OS_ARCH:=386
 endif
 OS_RELEASE_NAME:=$(shell lsb_release -i -s)
 endif
@@ -12,16 +14,29 @@ endif
 ifeq ($(ORIGN_OS_NAME),Darwin)
 OS_NAME:=mac
 OS_RELEASE_NAME:=darwin
+OS_ARCH:=amd64
 endif
 
 ifeq ($(filter CYGWIN%,$(ORIGN_OS_NAME)),$(ORIGN_OS_NAME))
 ifeq ($(shell uname -m),x86_64)
 OS_NAME:=cygwin64
+OS_ARCH:=amd64
 else
 OS_NAME:=cygwin
+OS_ARCH:=386
+endif
+OS_RELEASE_NAME:=cygwin
 endif
 
-OS_RELEASE_NAME:=$(OS_NAME)
+ifeq ($(filter MSYS_%,$(ORIGN_OS_NAME)),$(ORIGN_OS_NAME))
+ifeq ($(shell uname -m),x86_64)
+OS_NAME:=mingw64
+OS_ARCH:=amd64
+else
+OS_NAME:=mingw
+OS_ARCH:=386
+endif
+OS_RELEASE_NAME:=mingw
 endif
 
 ifeq ($(OS_NAME),)
