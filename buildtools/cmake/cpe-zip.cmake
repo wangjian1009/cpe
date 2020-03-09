2cde36794fcaf1bdc53e45740750959f4aca51a9
Lokiwang <wangjian1009@gmail.com>
2020-03-09 17:04:55 +0800

    增加zip模块

---
+++ buildtools/cmake/cpe-zip.cmake
@@ -0,0 +1,17 @@
+set(cpe_zip_base ${CMAKE_CURRENT_LIST_DIR}/../../zip)
+
+set(cpe_zip_source
+  ${cpe_zip_base}/src/ioapi.c
+  ${cpe_zip_base}/src/unzip.c
+  ${cpe_zip_base}/src/zip.c
+  ${cpe_zip_base}/src/zip_file.c
+  ${cpe_zip_base}/src/zip_dir.c
+  )
+
+add_library(cpe_zip STATIC ${cpe_zip_source})
+
+set_property(TARGET cpe_zip PROPERTY INCLUDE_DIRECTORIES
+    ${CMAKE_CURRENT_LIST_DIR}/../../pal/include
+    ${CMAKE_CURRENT_LIST_DIR}/../../utils/include
+    ${cpe_zip_base}/include
+  )
