6d0c228e4c273c25e147533d17b26ba4a79d97cd
Loki <wangjian1009@gmail.com>
2021-06-30 17:50:07 +0800

    update compiledb

--- buildtools/support-compiledb-cmake.mk
+++ buildtools/support-compiledb-cmake.mk
@@ -1,8 +1,8 @@
 .PHONY: compiledb
 
 compiledb:
-	mkdir -p $(COMPILEDB_ROOT)/$(if $(COMPILEDB_OUTPUT),$(COMPILEDB_OUTPUT),build/cmake) \
-	&& cd $(COMPILEDB_ROOT)/$(if $(COMPILEDB_OUTPUT),$(COMPILEDB_OUTPUT),build/cmake) \
-    && $(if $(CMAKE),$(CMAKE),cmake) $(COMPILEDB_ROOT) -Wno-dev -BDebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES $(COMPILEDB_CMAKE_ARGS) \
+	$(if $(CMAKE),$(CMAKE),cmake) $(COMPILEDB_ROOT) \
+		-B$(COMPILEDB_ROOT)/$(if $(COMPILEDB_OUTPUT),$(COMPILEDB_OUTPUT),build/cmake) \
+		-Wno-dev -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES $(COMPILEDB_CMAKE_ARGS) \
 	&& rm -f $(COMPILEDB_ROOT)/compile_commands.json \
-	&& ln -s $(if $(COMPILEDB_OUTPUT),$(COMPILEDB_OUTPUT),build/cmake)/Debug/compile_commands.json $(COMPILEDB_ROOT)/compile_commands.json
+	&& ln -s $(if $(COMPILEDB_OUTPUT),$(COMPILEDB_OUTPUT),build/cmake)/compile_commands.json $(COMPILEDB_ROOT)/compile_commands.json
